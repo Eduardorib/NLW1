a01e653f4dc560cdaae9fb1147ef317a36ae2362
Eduardo Ribeiro <eduardoribeirogon@gmail.com>
2021-02-11 18:14:15 -0300

    initial commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,71 @@
+// comments
+// document.write("Hello")
+
+// variáveis, tipos de dados
+
+//let myvar = "he"
+//const myconst = "he"
+
+//document.write(myconst + myvar)
+
+//string
+/*const s1 = "Isso é uma string"
+const s2 = 'Isso também é uma string'
+conts s3 = `Isso é uma string também`
+
+document.write(s1)*/
+
+//number
+/*const n1 = 1
+const n2 = 12
+document.write(n1 + n2)*/
+
+//boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+//document.write(bTrue)
+
+//objeto possuem propriedades e funcionalidades
+/* const pessoa = {
+ altura: "1,80m",
+  idade : 24,
+  solteiro: true,
+  correr(){
+    document.write("executar uma grande logica de correr")
+  }
+}
+
+pessoa.correr() */
+
+// Array - Vetores
+/*const colecaodebolinhas = ["blue", "green", 1, {name:"my name"}]
+
+document.write(colecaodebolinhas[3].name)
+*/
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+//registrar função
+/*function saymyname(name){
+  document.write(name)
+}
+//executar
+saymyname("Douglas")
+*/
+
+//condicionais
+/*
+const notaFinal = 7
+
+if( notaFinal < 5 ) {
+   document.write("Reprovado")
+} else{
+   document.write("Aprovado")
+}
+*/
+
+//loop - repetições
+/*
+for(i = 0; i <10; i++) {
+  document.write(`<p>${i}</p>`)
+}
+*/
